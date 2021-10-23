dff8da7c74fa7fd58de984d5390e3498e9167eb4
Hlemusv <80725116+Hlemusv@users.noreply.github.com>
2021-10-23 17:46:37 -0500

    prueba

--- form4.js
+++ form4.js
@@ -40,6 +40,6 @@ function validar_contrasenas(contrasena1, contrasena2) {
     return true;
   }
 }
-
+dfsd;
 module.exports.validar_nombre = validar_nombre;
 module.exports.validar_contrasenas = validar_contrasenas;
